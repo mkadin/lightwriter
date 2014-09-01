17d18d1f6944f4be0bd2eb09c9e7dfc5cd15bacb
Mike Kadin <kadin@uber.com>
2014-08-31 18:26:00 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='LightWriter',
-    version='1.0.1',
+    version='1.0.2',
     author='Mike Kadin',
     author_email='michaelkadin@gmail.com',
     packages=find_packages(),
